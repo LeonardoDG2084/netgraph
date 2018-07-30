093dc70cd2f44409091ca56a482eb7f940c26a8a
tvt01789 <leonardodg2084@gmail.com>
2018-07-30 15:09:30 -0300

    Add file netgraph

--- netgraph.py
+++ netgraph.py
@@ -5,19 +5,19 @@ import os
 import argparse
 
 arguments = argparse.ArgumentParser(prog='netgraph',
-                                    usage=' %(prog)s -f netstat.out -p 22,80' \
+                                    usage=' %(prog)s -f netstat.out -p 22,80 ' \
                                           '-ep 445,3389')
 
 arguments.add_argument('-f', '--netstat-file', required=True,
                        help='netstat output file', dest='inputNetstatFile')
 
-arguments.add_argument('-ip', '--include-ports', required=True,
+arguments.add_argument('-ip', '--include-ports', required=False,
                        help='Include ports', dest='includePorts')
 
-arguments.add_argument('-ep', '--exclude-ports', required=True,
+arguments.add_argument('-ep', '--exclude-ports', required=False,
                        help='Exclude ports', dest='includePorts')
 
 result = arguments.parse_args()
 
-print(result.input-netstat-file)
+print(result.inputNetstatFile)
 
