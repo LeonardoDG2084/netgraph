c5ea948a6d0bbac9b2e4c20a59f7bbadf2e10098
tvt01789 <leonardodg2084@gmail.com>
2018-07-30 14:44:06 -0300

    netgraph file!

---
+++ netgraph.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import argparse
+
+arguments = argparse.ArgumentParser(prog='netgraph',
+                                    usage=' %(prog)s -f netstat.out -p 22,80' \
+                                          '-ep 445,3389')
+
+arguments.add_argument('-f', '--netstat-file', required=True,
+                       help='netstat output file', dest='inputNetstatFile')
+
+arguments.add_argument('-ip', '--include-ports', required=True,
+                       help='Include ports', dest='includePorts')
+
+arguments.add_argument('-ep', '--exclude-ports', required=True,
+                       help='Exclude ports', dest='includePorts')
+
+result = arguments.parse_args()
+
+print(result.input-netstat-file)
+
